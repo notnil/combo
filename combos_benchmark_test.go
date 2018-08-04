cd117101c6e12d63dc4da5eb12efcae6f402411f
Kishan B <bkishan@thoughtworks.com>
2018-08-04 19:18:27 +0530

    Add more test cases and formatting corrections

--- combos_benchmark_test.go
+++ combos_benchmark_test.go
@@ -6,14 +6,14 @@ import (
 	"github.com/notnil/combos"
 )
 
-func Benchmark10C2(b *testing.B)     { benchmarknCr(10, 2, b) }
+func Benchmark10C2(b *testing.B) { benchmarknCr(10, 2, b) }
 
-func Benchmark100C2(b *testing.B)     { benchmarknCr(100, 2, b) }
+func Benchmark100C2(b *testing.B) { benchmarknCr(100, 2, b) }
 
-func Benchmark1000C2(b *testing.B)     { benchmarknCr(1000, 2, b) }
+func Benchmark1000C2(b *testing.B) { benchmarknCr(1000, 2, b) }
 
 func benchmarknCr(n int, r int, b *testing.B) {
-	for i := 0 ; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		combos.New(n, r)
 	}
 }
